8308f1cfb3285ff342c7ecbdc5c4123326573157
glabrie <ghil@hey.com>
2025-04-21 08:44:07 -0400

    rotation included

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import Player
+from player import *
 
 def main():
     pygame.init()
@@ -13,11 +13,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = clock.tick(60) / 1000.0
         
         pygame.Surface.fill(screen, "black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
+  
 
 if __name__ == "__main__":
     main()
