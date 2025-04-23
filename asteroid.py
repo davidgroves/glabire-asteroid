19bd24625d52009fcd60413d5b8dabf5f943a71a
glabrie <ghil@hey.com>
2025-04-23 14:41:50 -0400

    finished the official course

--- asteroid.py
+++ asteroid.py
@@ -1,20 +1,34 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import WHITE
+from constants import ASTEROID_MIN_RADIUS
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super.()__init__(x, y, radius)
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, 
-                           WHITE, 
-                           self.position,
-                           self.radius, 
-                           width=2
-                           )
-        
-    def update(self, dt, velocity):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.velocity += forward * self.velocity * dt
-        self.velocity += self.velocity * dt
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        old_radius = self.radius
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            rand_angle = random.uniform(20, 50)
+
+            new_velocity1 = self.velocity.rotate(rand_angle)
+            new_velocity2 = self.velocity.rotate(-rand_angle)
+
+            new_radius = old_radius - ASTEROID_MIN_RADIUS
+
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid1.velocity = new_velocity1 * 1.2
+
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid2.velocity = new_velocity2 * 1.2
--- circleshape.py
+++ circleshape.py
@@ -1,27 +1,24 @@
 import pygame
 
+
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
-        # The "image" attribute is required by pygame Sprites, even if you're not using it.
-        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)  # Create a transparent surface
-        self.rect = self.image.get_rect()  # Define the "rect" attribute
-        self.rect.center = (x, y)  # Position the rect at the initial position
-
     def draw(self, screen):
-        # sub-classes must override
+        # must override
         pass
 
     def update(self, dt):
-        # sub-classes must override
+        # must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
